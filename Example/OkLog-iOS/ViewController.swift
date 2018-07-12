6f8ac8860d586a433a40bbd6c16d0c60b77c0113
Diego Trevisan Lara <diego@trevisa.nl>
2018-07-12 08:41:09 -0300

    Shared sample requests between iOS and macOS targets.

--- Example/OkLog-iOS/ViewController.swift
+++ Example/OkLog-iOS/ViewController.swift
@@ -7,78 +7,23 @@
 //
 
 import UIKit
-import OkLog
-import Alamofire
 
 class ViewController: UIViewController {
     
-    let session = URLSession(configuration: .default)
-    
-    private func makeRequest(url: URL, method: String, parameters: [String: Any]?) -> URLRequest {
-        var request = URLRequest(url: url)
-        request.httpMethod = method
-        request.allHTTPHeaderFields = Constants.headerFields
-        
-        if let parameters = parameters {
-            let data = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-            request.httpBody = data
-        }
-        
-        return request
-    }
-    
     @IBAction private func logGet() {
-        
-        let url = URL(string: "\(Constants.urlBase)\(Constants.urlGet)")!
-        let request = makeRequest(url: url, method: "GET", parameters: nil)
-        
-        OkLog.willSend(request)
-        
-        session.dataTask(with: request) { (data, response, error) in
-            OkLog.log(request: request, response: response, data: data)
-        }.resume()
-        
+        Client().logGet()
     }
     
     @IBAction private func logPost() {
-        
-        let url = URL(string: "\(Constants.urlBase)\(Constants.urlPost)")!
-        let request = makeRequest(url: url, method: "POST", parameters: ["id": 1, "name": "John Johnson"])
-        
-        session.dataTask(with: request) { (data, response, error) in
-            OkLog.log(request: request, response: response, data: data, shortenUrl: false)
-        }.resume()
-        
+        Client().logPost()
     }
     
     @IBAction private func logPut() {
-        
-        let request = Alamofire.request("\(Constants.urlBase)\(Constants.urlPut)",
-            method: .put,
-            parameters: ["id": 2, "name": "James Smith"],
-            encoding: JSONEncoding.default,
-            headers: Constants.headerFields)
-        
-        OkLog.willSend(request)
-        
-        request.responseJSON { response in
-            OkLog.log(response, shortenUrl: false)
-        }
-        
+        Client().logPut()
     }
     
     @IBAction private func logDelete() {
-        
-        let request = Alamofire.request("\(Constants.urlBase)\(Constants.urlDelete)",
-            method: .delete,
-            parameters: ["id": 3, "name": "John Appleseed"],
-            encoding: JSONEncoding.default,
-            headers: Constants.headerFields)
-        
-        request.responseJSON { response in
-            OkLog.log(response)
-        }
-        
+        Client().logDelete()
     }
 
 }
--- Example/OkLog-macOS/ViewController.swift
+++ Example/OkLog-macOS/ViewController.swift
@@ -7,71 +7,23 @@
 //
 
 import Cocoa
-import OkLog
 
 class ViewController: NSViewController {
-
-    let session = URLSession(configuration: .default)
-    
-    private func makeRequest(url: URL, method: String, parameters: [String: Any]?) -> URLRequest {
-        var request = URLRequest(url: url)
-        request.httpMethod = method
-        request.allHTTPHeaderFields = Constants.headerFields
-        
-        if let parameters = parameters {
-            let data = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-            request.httpBody = data
-        }
-        
-        return request
-    }
     
     @IBAction private func logGet(sender: NSButton) {
-        
-        let url = URL(string: "\(Constants.urlBase)\(Constants.urlGet)")!
-        let request = makeRequest(url: url, method: "GET", parameters: nil)
-        
-        OkLog.willSend(request)
-        
-        session.dataTask(with: request) { (data, response, error) in
-            OkLog.log(request: request, response: response, data: data)
-        }.resume()
-        
+        Client().logGet()
     }
     
     @IBAction private func logPost(sender: NSButton) {
-        
-        let url = URL(string: "\(Constants.urlBase)\(Constants.urlPost)")!
-        let request = makeRequest(url: url, method: "POST", parameters: ["id": 1, "name": "John Johnson"])
-        
-        session.dataTask(with: request) { (data, response, error) in
-            OkLog.log(request: request, response: response, data: data, shortenUrl: false)
-        }.resume()
-        
+        Client().logPost()
     }
     
     @IBAction private func logPut(sender: NSButton) {
-        
-        let url = URL(string: "\(Constants.urlBase)\(Constants.urlPut)")!
-        let request = makeRequest(url: url, method: "POST", parameters: ["id": 2, "name": "James Smith"])
-        
-        OkLog.willSend(request)
-        
-        session.dataTask(with: request) { (data, response, error) in
-            OkLog.log(request: request, response: response, data: data, shortenUrl: false)
-        }.resume()
-        
+        Client().logPut()
     }
     
     @IBAction private func logDelete(sender: NSButton) {
-        
-        let url = URL(string: "\(Constants.urlBase)\(Constants.urlDelete)")!
-        let request = makeRequest(url: url, method: "POST", parameters: ["id": 3, "name": "John Appleseed"])
-        
-        session.dataTask(with: request) { (data, response, error) in
-            OkLog.log(request: request, response: response, data: data)
-        }.resume()
-        
+        Client().logDelete()
     }
 
 }
