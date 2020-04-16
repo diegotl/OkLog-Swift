fabf10914640ca5945b53d379fc4526679b2d5bc
Diego Trevisan Lara <diego@trevisa.nl>
2020-04-16 14:44:34 +0200

    Fixed tests.

--- Tests/OkLogTests/OkLogTests.swift
+++ Tests/OkLogTests/OkLogTests.swift
@@ -10,13 +10,10 @@ final class OkLogTests: XCTestCase {
         let url = URL(string: urlString)!
         let request = URLRequest(url: url)
         
-        OkLog.willSend(request)
         URLSession.shared.dataTask(with: request) { data, response, error in
-            
             let logUrl = OkLog.getUrl(request: request, response: response, data: data)
             XCTAssertFalse(logUrl.replacingOccurrences(of: "http://oklog.responseecho.com/v1/r/", with: "").isEmpty)
             expectation.fulfill()
-            
         }.resume()
         
         wait(for: [expectation], timeout: 10)
